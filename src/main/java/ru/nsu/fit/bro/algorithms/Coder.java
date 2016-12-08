16f2ce9221d40f3618d63234e29afc536fcc2021
Kondyrev Dmitriy <dkondyrev@gmail.com>
2016-12-08 15:44:56 +0700

    Algorithms: function signatures changed

--- src/main/java/ru/nsu/fit/bro/algorithms/Coder.java
+++ src/main/java/ru/nsu/fit/bro/algorithms/Coder.java
@@ -1,5 +1,7 @@
 package ru.nsu.fit.bro.algorithms;
 
+import java.io.IOException;
+
 /**
  * This class provides an interface for embedding a text into an image
  */
@@ -11,8 +13,9 @@ public class Coder {
      * @param image the source image
      * @param key the secret key which is known only to sender and recipient
      * @return the image with the embedded text
+     * @throws IOException if an error occurs during reading or writing the image
      */
-    public byte[] code(String message, byte[] image, long key) {
+    public byte[] code(String message, byte[] image, long key) throws IOException {
         return null;
     }
 }
--- src/main/java/ru/nsu/fit/bro/algorithms/Decoder.java
+++ src/main/java/ru/nsu/fit/bro/algorithms/Decoder.java
@@ -1,7 +1,9 @@
 package ru.nsu.fit.bro.algorithms;
 
+import java.io.IOException;
+
 /**
- * This class provides an interface for extracting a text into an image
+ * This class provides an interface for extracting a text from an image
  */
 public class Decoder {
 
@@ -10,8 +12,9 @@ public class Decoder {
      * @param image the image with the embedded text
      * @param key the secret key which is known only to sender and recipient
      * @return the extracted message
+     * @throws IOException if an error occurs during reading the image
      */
-    public String decode(byte[] image, long key){
+    public String decode(byte[] image, long key) throws IOException {
         return null;
     }
 }
