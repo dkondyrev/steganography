08de794b28f34c2dd1709ad68b86b0dae8d53199
Яна Жаркова < >
2016-12-12 00:36:36 +0700

    WEB-UI: added image loading

--- src/main/java/ru/nsu/fit/bro/client/WebController.java
+++ src/main/java/ru/nsu/fit/bro/client/WebController.java
@@ -3,7 +3,6 @@ package ru.nsu.fit.bro.client;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WebController {
@@ -13,5 +12,4 @@ public class WebController {
 
         return "stenography";
     }
-
 }
