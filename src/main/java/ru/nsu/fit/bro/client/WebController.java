02fdeee9e8ae7306902572b5a54cdb3dbdaa6a59
kushner <kushner.k.g@gmail.com>
2016-12-13 04:59:06 +0700

    imolement encoding interaction

--- src/main/java/ru/nsu/fit/bro/client/WebController.java
+++ src/main/java/ru/nsu/fit/bro/client/WebController.java
@@ -1,10 +1,21 @@
 package ru.nsu.fit.bro.client;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import ru.nsu.fit.bro.rest.model.StenographyImageResponse;
+
+import java.io.*;
+import java.util.Base64;
 
 @Controller
 public class WebController {
@@ -26,32 +37,35 @@ public class WebController {
     }
 
     @RequestMapping(value = "/stenography/coder-result", method = RequestMethod.POST)
-    public String providedCoderResultPage(@RequestParam("name") String name, Model model) {
+    public String providedCoderResultPage(@RequestParam("message") String message,
+                                          @RequestParam("number") String number,
+                                          @RequestParam("image") MultipartFile image,
+                                          Model model) throws IOException {
+        if (!image.isEmpty()) {
+
+            final String uri = "http://localhost:8080/rest/stenography/encode";
+
+            byte[] bytes = image.getBytes();
+
+            MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+            headers.add("message", message);
+            headers.add("key", number);
+
+            HttpEntity<String> entity = new HttpEntity<String>(Base64.getEncoder().encodeToString(bytes), headers);
+            RestTemplate restTemplate = new RestTemplate();
+            StenographyImageResponse response = restTemplate.postForObject(uri, entity, StenographyImageResponse.class);
+
+            byte[] result = Base64.getDecoder().decode(response.getImage());
+            FileOutputStream fos = new FileOutputStream("image.bmp");
+            fos.write(result);
+            fos.close();
+        } else {
+            //return "Вам не удалось загрузить " + name + " потому что файл пустой.";
+        }
+
         return "stenography/coder-result";
     }
 
-//    <form name="send" enctype="multipart/form-data" action="/stenography/coder-result.html" method="post" onsubmit="return validate_form ( );">
-//    <p>
-//    <b>Поле сообщения:</b><br />
-//    <textarea id="crypto_text" rows="2" style="width: 98%; resize: none"></textarea><br />
-//    </p>
-//    <p><br/>
-//    <b>Случайное число:</b><br/>
-//    <label for="random_number_field"></label><input id="random_number_field" type="text" maxlength="25" size="40" /><br />
-//    <input type="button" onclick="random_number()" value="Сгенерировать число"/>
-//    </p>
-//    <center>
-//    <p><br/>
-//    <b>Здесь должна быть ваша картинка. В нее мы зашифруем ваше послание:</b><br/>
-//    <input id="file_with_img" type="file" accept="image/*" style="color:grey;" onchange="loadImage(this)">
-//    </input><br/><br/>
-//    <img id="myimage"/><br/><br/>
-//    </p>
-//    <p>
-//    <input id="send" type="submit" value="Зашифровать послание" />
-//    </p>
-//    </center>
-//    </form>
     @RequestMapping("/stenography/decoder-result")
     public String providedDecoderResultPage(Model model) {
         return "stenography/decoder-result";
